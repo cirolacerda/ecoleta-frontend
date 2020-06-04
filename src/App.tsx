32c6b8815cdf589540bbb5a8f1b77f9648a625b8
Ciro Botelho Lacerda <cirolacerda@gmail.com>
2020-06-04 15:50:49 -0300

    Subindo projeto dia 3

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+// JSX: Sintaxe de XML dentro do JavaScript
+
+import Header from './Header';
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+
+      <h1>Conteúdo da aplicação</h1>
+    </div>
+    
+  );
+}
+
+export default App;
